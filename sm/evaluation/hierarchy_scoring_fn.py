9a8546d5267c4b35279b2d2a9b0d06dfb1cde417
Binh Vu <binh@toan2.com>
2022-03-04 14:24:25 +0700

    [v3.2.0] change hierarchical scoring function interface & construction to prevent construct incomplete tree & to calculate correct shortest distances.

--- sm/evaluation/hierarchy_scoring_fn.py
+++ sm/evaluation/hierarchy_scoring_fn.py
@@ -1,52 +1,74 @@
 import math
-from typing import Dict, Callable, Set, Iterable
-
+from typing import Dict, Callable, List, Mapping, Set, Iterable, Tuple
+from dataclasses import dataclass
 from sm.evaluation.sm_metrics import ScoringFn
+from tqdm import tqdm
+
 
 ItemParents = Dict[str, Iterable[str]]
 
 
+@dataclass
 class HierarchyScoringFn(ScoringFn):
-    def __init__(self, item_parents: ItemParents, get_item_uri: Callable[[str], str]):
-        # mapping from the item to its parents
-        self.i2i: Dict[str, Dict[str, int]] = {p: {} for p in item_parents}
-        for p in item_parents:
-            self.build_i2i(p, p, 1, item_parents, set())
-        self.i2i = {
-            get_item_uri(p): {get_item_uri(pp): d for pp, d in ppd.items()}
-            for p, ppd in self.i2i.items()
-        }
+    # mapping from the item to its parents and their distances
+    # 1 is direct parent, 2 is grandparent, etc.
+    item2parents: Mapping[str, Mapping[str, int]]
 
-    def build_i2i(
-        self,
-        origin_item: str,
-        current_item: str,
-        distance: int,
-        item_parents: ItemParents,
-        visited: Set[str],
+    @staticmethod
+    def construct(
+        items: List[str],
+        get_item_parents: Callable[[str], Iterable[str]],
+        get_item_uri: Callable[[str], str],
+        verbose: bool = False,
     ):
-        if current_item in visited or current_item not in item_parents:
-            return
+        """Create a HierarchyScoringFn from a list of items
 
-        visited.add(current_item)
-        for pp in item_parents[current_item]:
-            if pp in self.i2i[origin_item]:
-                self.i2i[origin_item][pp] = min(self.i2i[origin_item][pp], distance)
-            else:
-                self.i2i[origin_item][pp] = distance
-            self.build_i2i(origin_item, pp, distance + 1, item_parents, visited)
+        Args:
+            items: List of items to build the scoring function
+            get_item_parents: Function that returns the parents of an item
+            get_item_uri: Function that returns the URI of an item
+        """
+        item2parents = {item: {} for item in items}
+
+        for item in tqdm(items, disable=not verbose):
+            # a mapping of visited item to the distance that we encounter it
+            visited = {}
+            stack: List[Tuple[str, int]] = [
+                (parent, 1) for parent in get_item_parents(item)
+            ]
+            while len(stack) > 0:
+                node, distance = stack.pop()
+                if node in visited and distance >= visited[node]:
+                    # we have visited this node before and since last time we visit
+                    # the previous route is shorter, so we don't need to visit it again
+                    continue
+
+                visited[node] = distance
+                item2parents[item][node] = min(
+                    distance, item2parents[item].get(node, float("inf"))
+                )
+
+                for parent in get_item_parents(node):
+                    stack.append((parent, distance + 1))
+
+        return HierarchyScoringFn(
+            {
+                get_item_uri(k): {get_item_uri(k2): v2 for k2, v2 in v.items()}
+                for k, v in item2parents.items()
+            }
+        )
 
     def get_match_score(self, pred_item: str, target_item: str):
         if pred_item == target_item:
             return 1.0
-        if pred_item in self.i2i[target_item]:
+        if pred_item in self.item2parents[target_item]:
             # pred_predicate is the parent of the target
-            distance = self.i2i[target_item][pred_item]
+            distance = self.item2parents[target_item][pred_item]
             if distance > 5:
                 return 0.0
             return math.pow(0.8, distance)
-        if target_item in self.i2i[pred_item]:
-            distance = self.i2i[pred_item][target_item]
+        if target_item in self.item2parents[pred_item]:
+            distance = self.item2parents[pred_item][target_item]
             if distance > 3:
                 return 0.0
             return math.pow(0.7, distance)
