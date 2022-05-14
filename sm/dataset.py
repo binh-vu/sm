0a97ba0b81c092ceae5b68caea3cb693d6e70622
Binh Vu <binh@toan2.com>
2022-05-13 21:11:41 -0700

    support zip compressed dataset

--- sm/dataset.py
+++ sm/dataset.py
@@ -1,6 +1,8 @@
 from dataclasses import dataclass
 from pathlib import Path
+from zipfile import ZipFile
 from typing import List, TypeVar, Generic, Union, Callable
+import orjson
 
 from sm.misc import deserialize_json, get_latest_path
 from sm.outputs import SemanticModel
@@ -18,15 +20,28 @@ def load(
     data_dir: Union[str, Path], table_deser: Callable[[dict], T]
 ) -> List[Example[T]]:
     """Load dataset from a folder. Assuming the following structure:
-    - descriptions: (containing semantic descriptions of tables)
-        - <table_fs_id>
-            - version.01.json
-            - version.02.json
-            - ...
-        - ...
-    - tables: (containing list of tables, the type of table depends on )
-        - <table_fs_id>.json
-        - ...
+
+    descriptions (containing semantic descriptions of tables)
+    ├── <table_fs_id>
+    │   ├── version.01.json
+    │   ├── version.02.json
+    │   └── ...
+        or
+    ├── <table_fs_id>.json
+    ├── ...
+    tables (containing list of tables, the type of table depends on )
+    ├── <table_fs_id>.json
+    ├── ...
+
+    We also support compressing formats such as .zip.
+    descriptions
+    ├── part-<num>.zip (no nested version folders)
+    │   ├── <table_fs_id>.json
+    |   |   ...
+    tables
+    ├── part-<num>.zip
+    │   ├── <table_fs_id>.json
+    |   |   ...
 
     Args:
         data_dir:
@@ -40,14 +55,52 @@ def load(
     for infile in sorted((data_dir / "tables").iterdir()):
         if infile.name.startswith("."):
             continue
-        assert infile.name.endswith(".json")
-        example_id = infile.stem
+        if infile.name.endswith(".json"):
+            example_id = infile.stem
 
-        table = table_deser(deserialize_json(infile))
-        raw_sms = deserialize_json(
-            get_latest_path(data_dir / f"descriptions/{example_id}/version.json")
-        )
-        sms = [SemanticModel.from_dict(sm) for sm in raw_sms]
+            table = table_deser(deserialize_json(infile))
 
-        examples.append(Example(sms=sms, table=table))
+            if (data_dir / f"descriptions/{example_id}").exists():
+                desc_file = get_latest_path(
+                    data_dir / f"descriptions/{example_id}/version.json"
+                )
+                assert desc_file is not None
+            else:
+                desc_file = data_dir / f"descriptions/{example_id}.json"
+                assert desc_file.exists()
+
+            raw_sms = deserialize_json(desc_file)
+            sms = [SemanticModel.from_dict(sm) for sm in raw_sms]
+
+            examples.append(Example(sms=sms, table=table))
+        elif infile.name.endswith(".zip"):
+
+            part = {}
+            with ZipFile(infile, mode="r") as zf:
+                for file in zf.infolist():
+                    if not file.filename.endswith(".json"):
+                        continue
+
+                    table_id = Path(file.filename).stem
+                    with zf.open(file, mode="r") as f:
+                        table = table_deser(orjson.loads(f.read()))
+                    part[table_id] = table
+
+            lst = []
+            with ZipFile(data_dir / "descriptions" / infile.name, mode="r") as zf:
+                for file in zf.infolist():
+                    table_id = Path(file.filename).stem
+                    if table_id not in part:
+                        continue
+
+                    assert file.filename.endswith(".json")
+                    with zf.open(file, mode="r") as f:
+                        sms = [
+                            SemanticModel.from_dict(sm) for sm in orjson.loads(f.read())
+                        ]
+                        lst.append(Example(sms=sms, table=part[table_id]))
+
+            assert len(lst) == len(part)
+            examples.extend(lst)
+
     return examples
