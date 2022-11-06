43ab54dddcd40ab3f7ff9d261bbde5193aa6fcdf
Binh Vu <binh@toan2.com>
2022-11-06 15:53:27 -0800

    remove deprecated modules from prelude

--- sm/misc/prelude.py
+++ sm/misc/prelude.py
@@ -1,8 +1,5 @@
 from sm.misc.fn_cache import *
-from sm.misc.deser import *
 from sm.misc.funcs import *
-from sm.misc.exp_manager import *
-from sm.misc.logger import *
 from sm.misc.bijection import Bijection
 from sm.misc.matrix import Matrix
 
