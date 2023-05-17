bc5fcfb012255bef313239bb680560f2b5ffea4c
Binh Vu <binh@toan2.com>
2023-05-16 21:27:12 -0700

    update rsoup

--- sm/inputs/context.py
+++ sm/inputs/context.py
@@ -1,7 +1,7 @@
 from __future__ import annotations
 from dataclasses import dataclass, field
 from typing import List, Optional
-from rsoup.rsoup import ContentHierarchy
+from rsoup.core import ContentHierarchy
 from sm.inputs.link import EntityId
 
 
