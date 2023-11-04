d84370f2d3586585ec0cd2388653527511be5924
Binh Vu <binh@toan2.com>
2023-11-03 22:34:56 -0700

    add helper to register kgns

--- sm/namespaces/utils.py
+++ sm/namespaces/utils.py
@@ -3,6 +3,8 @@ from enum import Enum
 from sm.namespaces.namespace import KnowledgeGraphNamespace
 from sm.namespaces.wikidata import WikidataNamespace
 
+registered_kgns: dict[str, KnowledgeGraphNamespace] = {}
+
 
 class KGName(str, Enum):
     Wikidata = "wikidata"
@@ -12,4 +14,16 @@ class KGName(str, Enum):
 def get_kgns(kgname: KGName) -> KnowledgeGraphNamespace:
     if kgname == "wikidata":
         return WikidataNamespace.create()
+    if kgname in registered_kgns:
+        return registered_kgns[kgname]
     raise NotImplementedError(kgname)
+
+
+def register_kgns(kgname: str, kgns: KnowledgeGraphNamespace):
+    global registered_kgns
+    registered_kgns[kgname] = kgns
+
+
+def has_kgns(kgname: str) -> bool:
+    global registered_kgns
+    return kgname in registered_kgns
